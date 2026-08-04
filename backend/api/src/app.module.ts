ce761c2ee7b0117aa7d4032d924f9657749ef46b
Robson Nascimento <robson.dsnascimento@gmail.com>
2026-08-04 16:27:09 -0300

    chore: ajusta configuracao prisma e estrutura inicial pessoa

--- backend/api/src/app.module.ts
+++ backend/api/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './modules/health/health.module';
+import { DatabaseModule } from './database/database.module';
+import { PersonModule } from './modules/person/person.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [HealthModule, DatabaseModule, PersonModule],
 })
 export class AppModule {}
